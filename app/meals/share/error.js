127e06f2d0d4c6a018c4e40ec87da033d4eb1499
dalia <daliabalmus@gmail.com>
2025-09-09 16:11:34 +0300

    added server side validation

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+﻿"use client";
+
+export default function Error() {
+  return (
+    <main className={"error"}>
+      <h1>An error occurred!</h1>
+      <p>Failed to create meal.</p>
+    </main>
+  );
+}
